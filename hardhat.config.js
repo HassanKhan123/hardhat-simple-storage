524faca0164f4edb0911022b2fdf0cdc3eeb1e88
HassanKhan123 <hk2602809@gmail.com>
2022-06-21 18:37:39 +0500

    added: custom task to fetch latest blocknumber

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
+
 const { PRIVATE_KEY, RINKEBY_RPC_URL, ETHERSCAN_API_KEY } = require("./secret");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+require("./tasks/block-number");
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
